da08b64f8a2b890d4e8b7f9a8d8d83374fb8201c
Kacper Kminikowski <90308953+Kvper@users.noreply.github.com>
2021-09-27 08:04:09 +0000

    classes

--- script.js
+++ script.js
@@ -1,52 +1,54 @@
-var glass, wood, swidth
+var swidth
 
 swidth = 800;
 
 class Woodrec{
-  constructor(x, y, vx, vy, width, height, color){
-    this.color = "298f12"
+  constructor(x, y, w, h, vx, vy, c){
     this.x = x;
     this.y = y;
+    this.h = h;
+    this.w = w;
+    this.c = c;
     this.vx = vx;
     this.vy = vy;
-    this.height = height;
-    this.width = width; 
+    
   }
 
   draw(){
-    rect(this.x, this.y, this.width, this.height);
+    rect(this.x, this.y,this.w, this.h, this.c, this.vx, this.vy);
   }
 
 }
 
-function draw(){
-  fill("#298f12");
-  rect(0,350,swidth,50);
-  
-  fill("#b1651f")
-  rect(500, 335, 210, 15)
-  rect(520, 250, 10, 85)
-  rect(600, 250, 10, 85)
-  rect(680, 250, 10, 85)
-  rect(520, 240, 85, 10)
-  rect(605, 240, 85, 10)
-  rect(530, 150, 150, 10)
-  rect(100,265, 15, 85)
-  ellipse(555, 130, 40, 40)
-
-
-  fill("#ccfffe")
-  rect(550, 160, 10, 80)
-  rect(650, 160, 10, 80)
-  ellipse(555, 130, 40, 40)
-
-}
-
+var rec1, rec2, rec3, rec4
 
 function setup() {
 	createCanvas(swidth, 400);
-  background("cyan");
-  
-  Rec1 = new Woodrec(500, 335, 210, 15)
+  rec1 = new Woodrec(500, 335, 210, 15, );
  
 }
+
+function draw(){
+  background("cyan");
+  fill("#298f12");
+  
+  rect(0,350,swidth,50);
+  
+  rec1.draw();
+  rect(520, 250, 10, 85);
+  rect(600, 250, 10, 85);
+  rect(680, 250, 10, 85);
+  rect(520, 240, 85, 10);
+  rect(605, 240, 85, 10);
+  rect(530, 150, 150, 10);
+  rect(100,265, 15, 85);
+  ellipse(555, 130, 40, 40);
+
+
+  fill("#ccfffe");
+  rect(550, 160, 10, 80);
+  rect(650, 160, 10, 80);
+  ellipse(555, 130, 40, 40);
+
+}
+
